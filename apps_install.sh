7e4dc407287e41443fe0ba5549f6eb4fd17e6348
Rafael Alonso <rafaelalonsobarbosa@gmail.com>
2025-04-02 10:42:04 -0300

    feat: criado script para instalação e execução do projeto

--- apps_install.sh
+++ apps_install.sh
@@ -1,16 +1,19 @@
-#!/bin/bash
-printf "\n> Instalando as dependencias\n"
-yarn
-
 printf "\n> Instalando o back-end\n"
 cd ./backend
-yarn
+composer install
 
 printf "\n> Instalando o front-end\n"
 cd ../frontend
 yarn
 
+printf "\n> Rodando os containers\n"
+docker-compose up -d
 
-printf "\n> Iniciando Projeto\n"
-cd ../
-yarn start
+printf "\n> Aguardando a inicialização dos containers\n"
+sleep 5  # Dá um tempo para os containers subirem completamente
+
+printf "\n> Rodando as migrations e seeders dentro do container\n"
+docker exec -it mariah_api php artisan migrate:fresh --seed
+
+printf "\n> Rodando o server local dentro do container\n"
+docker exec -it mariah_api php artisan serve --host=0.0.0.0 --port=8000
