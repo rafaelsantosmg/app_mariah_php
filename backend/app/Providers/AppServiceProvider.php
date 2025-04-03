66c25f5f0dd1bc04af1d2e817a08de9d1cb97a37
Rafael Alonso <rafaelalonsobarbosa@gmail.com>
2025-04-02 22:20:42 -0300

    feat: adcionar a saleINterface no provider

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\Interfaces\ProductInterface;
+use App\Interfaces\SaleInterface;
 use App\Repositories\ProductRepository;
+use App\Repositories\SaleRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
   public function register(): void
   {
     $this->app->bind(ProductInterface::class, ProductRepository::class);
+    $this->app->bind(SaleInterface::class, SaleRepository::class);
   }
 
   /**
