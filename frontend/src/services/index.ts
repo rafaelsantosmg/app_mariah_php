4f6f5b5f7496b12127f19bde7a5ac32a28ba0159
Rafael Alonso <rafaelalonsobarbosa@gmail.com>
2025-04-03 12:45:13 -0300

    refactor: adicionado função para pegar o token pelo cookie

--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -1,10 +1,17 @@
 import axios from 'axios'
+import Cookies from "js-cookie";
+
+export const getAuthToken = () => {
+  return Cookies.get("auth_token"); // Obtém o token salvo no cookie
+};
+
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   headers: {
+    'Accept': 'application/json',
     'Content-Type': 'application/json',
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
+    'Authorization': `Bearer ${getAuthToken()}`,
   },
 })
 
