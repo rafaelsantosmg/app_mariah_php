4d8e09fac904eadd574344e0a4c37091753733fb
Rafael Alonso <rafaelalonsobarbosa@gmail.com>
2025-04-02 22:20:18 -0300

    refactor: removido o metodo update

--- backend/app/Interfaces/SaleInterface.php
+++ backend/app/Interfaces/SaleInterface.php
@@ -7,6 +7,5 @@ interface SaleInterface
   public function getAll();
   public function getById(int $id);
   public function create(array $data);
-  public function update(int $id, array $data);
   public function delete(int $id);
 }
