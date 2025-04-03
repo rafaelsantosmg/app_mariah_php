a3ac761ba3f1353c5123220ee3b6a3073957e775
Rafael Alonso <rafaelalonsobarbosa@gmail.com>
2025-04-03 12:56:48 -0300

    fix: correção do script de instalação

--- apps_install.sh
+++ apps_install.sh
@@ -1,9 +1,11 @@
 printf "\n> Instalando o back-end\n"
 cd ./backend
+mv .env.example .env
 composer install
 
 printf "\n> Instalando o front-end\n"
 cd ../frontend
+mv .env.example .env
 yarn
 
 printf "\n> Rodando os containers\n"
@@ -14,3 +16,5 @@ docker exec -it mariah_api php artisan migrate:fresh --seed
 
 printf "\n> Rodando o server local dentro do container\n"
 docker exec -it mariah_api php artisan serve --host=0.0.0.0 --port=8000
+
+xdg-open http://localhost:3000
